f66a2b6e1f7b92ee61d164b056d593a8d58b9e50
Léo Statkus <leo@statkus.fr>
2019-11-12 15:04:37 +0000

    Initial version of script

---
+++ button_soundboard.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from pygame import mixer
+import RPi.GPIO as GPIO
+
+def button_callback(channel):
+    print("Button was pushed!")
+    mixer.music.load("sounds/kaamelott/compote.mp3")
+    mixer.music.play()
+
+mixer.init()
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
