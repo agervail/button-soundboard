b2db71632947e80f8b87d643e9b594c5317e5750
Léo Statkus <leo@statkus.fr>
2019-11-12 15:59:33 +0000

    [DEV] Set GPIO input pull up resistor and detect falling edge

--- button_soundboard.py
+++ button_soundboard.py
@@ -12,7 +12,9 @@ mixer.init()
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.setup(10, GPIO.IN, GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10, GPIO.FALLING, callback=button_callback) # Setup event on pin 10 rising edge
+
 message = input("Press enter to quit\n\n") # Run until someone presses enter
+
 GPIO.cleanup() # Clean up
