44bc7f974cfb3c145703758bce0f450e495c4bfc
Léo Statkus <leo@statkus.fr>
2019-11-14 14:56:30 +0000

    [DEV] Update the script to handle multiple sounds bank

--- button_soundboard.py
+++ button_soundboard.py
@@ -5,23 +5,39 @@ from pygame import mixer
 import RPi.GPIO as GPIO
 import sys
 
-def button_callback(channel):
-	print(channel)
-	mixer.music.load(sounds[str(channel)])
-	mixer.music.play()
+sounds = []
+current_sounds_bank = 0
+special_button_1 = 3
+special_button_2 = 37
 
-if len(sys.argv) == 2:
-	with open(sys.argv[1], 'r') as f:
-		sounds = json.load(f)
+def button_callback(channel):
+	global current_sounds_bank
+
+	if (channel == special_button_1 and not GPIO.input(special_button_2)) or (channel == special_button_2 and not GPIO.input(special_button_1)):
+		current_sounds_bank += 1
+		current_sounds_bank %= len(sys.argv) - 1
+	else:
+		print(channel)
+		mixer.music.load(sounds[current_sounds_bank][str(channel)])
+		mixer.music.play()
+
+if len(sys.argv) >= 2:
+	for sounds_bank in range(len(sys.argv) - 1):
+		with open(sys.argv[sounds_bank + 1], 'r') as f:
+			sounds.append(json.load(f))
 
 	mixer.init()
 
 	GPIO.setwarnings(False) # Ignore warning for now
 	GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
-	for gpio in sounds:
-		GPIO.setup(int(gpio), GPIO.IN, GPIO.PUD_UP)
-		GPIO.add_event_detect(int(gpio), GPIO.FALLING, callback=button_callback, bouncetime=500)
+	used_gpios = []
+	for sounds_bank in sounds:
+		for gpio in sounds_bank:
+			if int(gpio) not in used_gpios:
+				GPIO.setup(int(gpio), GPIO.IN, GPIO.PUD_UP)
+				GPIO.add_event_detect(int(gpio), GPIO.FALLING, callback=button_callback, bouncetime=500)
+				used_gpios.append(int(gpio))
 
 	message = input("Press enter to quit\n\n") # Run until someone presses enter
 
